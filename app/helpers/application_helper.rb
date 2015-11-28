281440e5dd340d124b2fa0076f9ad196c494d806
Luis Manotas <lgerman@gmail.com>
2015-11-28 18:03:45 +0100

    Bootstrap Style for flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "alert"
+        "alert-warning"
+      when "notice"
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
